06339b8d0c4e15e86bd7faf8ff2c99df62f1f688
Shweta Mate <156800542+Shwetamate282@users.noreply.github.com>
2024-01-31 18:48:00 +0530

    Add files via upload

---
+++ 5 Que .c
@@ -0,0 +1,146 @@
+#include<stdio.h>
+main()
+
+{
+	char ans;
+	printf("1.What is the National Sweet of India?\n");
+	printf("A.Ladoo\n");
+	printf("B.Jalebi\n");
+	printf("C.Ras Malai\n");
+	printf("D.Gulab Jamun\n");
+	
+	printf("Enter Your Choise\n");
+	scanf(" %c",&ans);
+	switch(ans) // char
+	{
+		case'A':printf("Wrong Answer!");
+		break;
+		case'B':printf("Correct Answer!");
+		break;
+		case'C':printf("Wrong Answer!");
+		break;
+		case'D':printf("Wrong Answer!");
+		break;
+	    case'a':printf("Wrong Answer!");
+		break;
+		case'b':printf("Correct Answer!");
+		break;
+		case'c':printf("Wrong Answer!");
+		break;
+		case'd':printf("Wrong Answer!");
+		default:printf("Enter Valid Input");
+	}
+	
+	printf("\n\n2 2.Who is The National Animal of India?\n");
+	printf("A.Tiger\n");
+	printf("B.Lion\n");
+	printf("C.Monkey\n");
+	printf("D.Rabbit\n");
+	
+	printf("Enter Your Choise\n");
+	scanf(" %c",&ans);
+	switch(ans)
+	{
+		case'A':printf("Correct Answer!");
+		break;
+		case'B':printf("Wrong Answer!");
+		break;
+		case'C':printf("Wrong Answer!");
+		break;
+		case'D':printf("Wrong Answer!");
+		break;
+	    case'a':printf("Correct Answer!");
+		break;
+		case'b':printf("Wrong Answer!");
+		break;
+		case'c':printf("Wrong Answer!");
+		break;
+		case'd':printf("Wrong Answer!");
+		default:printf("Enter Valid Input");
+	}
+	
+	printf("\n\n3 3.What is The National Flower of India?\n");
+    printf("A.Lotus\n");
+	printf("B.Sunflower\n");
+	printf("C.Rose\n");
+	printf("D.Lilly\n");
+	
+	printf("Enter Your Choise\n");
+	scanf(" %c",&ans);
+	switch(ans)
+	{
+		case'A':printf("Correct Answer!");
+		break;
+		case'B':printf("Wrong Answer!");
+		break;
+		case'C':printf("Wrong Answer!");
+		break;
+		case'D':printf("Wrong Answer!");
+		break;
+	    case'a':printf("Correct Answer!");
+		break;
+		case'b':printf("Wrong Answer!");
+		break;
+		case'c':printf("Wrong Answer!");
+		break;
+		case'd':printf("Wrong Answer!");
+		default:printf("Enter Valid Input");
+	}
+	
+	printf("\n\n4 4.What is The National Sports Of India?\n");
+	printf("A.Cricket\n");
+	printf("B.Hockey\n");
+	printf("C.Kho-Kho\n");
+	printf("D.Kabbadi\n");
+	
+	printf("Enter Your Choise\n");
+	scanf(" %c",&ans);
+	switch(ans)
+	{
+		case'A':printf("Wrong Answer!");
+		break;
+		case'B':printf("Correct Answer!");
+		break;
+		case'C':printf("Wrong Answer!");
+		break;
+		case'D':printf("Wrong Answer!");
+		break;
+	    case'a':printf("Wrong Answer!");
+		break;
+		case'b':printf("Correct Answer!");
+		break;
+		case'c':printf("Wrong Answer!");
+		break;
+		case'd':printf("Wrong Answer!");
+		default:printf("Enter Valid Input");
+	}
+	
+	printf("\n\n5 5.What is The National Bird Of India?\n");
+	printf("A.Parrot\n");
+	printf("B.Sparrow\n");
+	printf("C.Peacock\n");
+	printf("D.Crow\n");
+	
+	printf("Enter Your Choise\n");
+	scanf(" %c",&ans);
+	switch(ans)
+	{
+		case'A':printf("Wrong Answer!");
+		break;
+		case'B':printf("Wrong Answer!");
+		break;
+		case'C':printf("Correct Answer!");
+		break;
+		case'D':printf("Wrong Answer!");
+		break;
+	    case'a':printf("Wrong Answer!");
+		break;
+		case'b':printf("Wrong Answer!");
+		break;
+		case'c':printf("Correct Answer!");
+		break;
+		case'd':printf("Wrong Answer!");
+		default:printf("Enter Valid Input");
+	}
+	
+}
